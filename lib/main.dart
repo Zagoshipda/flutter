205e69f6e51363a518803c8d1336f9a29b0be0cb
Zagoshipda <john713@hanmail.net>
2025-11-03 00:50:07 +0900

    Update : 7. Add navigation rail

--- lib/main.dart
+++ lib/main.dart
@@ -53,8 +53,80 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
   // Every widget defines a build() method which automatically called every time
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    // Every build() method must return a widget or (more typically) a nested tree of widgets
+    return LayoutBuilder(
+      // builder callback is called every time the constraints change
+      builder: (context, constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              // SafeArea ensures that its child is not obscured by a hardware notch or a status bar
+              SafeArea(
+                child: NavigationRail(
+                  extended:
+                      // true
+                      constraints.maxWidth >=
+                      600, // shows the labels next to the icons or not, responsively
+                  destinations: [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.home),
+                      label: Text('Home'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.favorite),
+                      label: Text('Favorites'),
+                    ),
+                  ],
+
+                  selectedIndex: selectedIndex,
+
+                  onDestinationSelected: (value) {
+                    print('selected: $value'); // debugging message
+                    setState(() {
+                      selectedIndex = value;
+                    });
+                  },
+                ),
+              ),
+
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: page,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
@@ -67,47 +139,42 @@ class MyHomePage extends StatelessWidget {
       icon = Icons.favorite_border;
     }
 
-    // Every build() method must return a widget or (more typically) a nested tree of widgets
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Text('A random AWESOME idea ? :'),
-            ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // The Text widget no longer refers to the whole appState, rather it only takes 'current' value
+          BigCard(pair: pair),
 
-            // The Text widget no longer refers to the whole appState, rather it only takes 'current' value
-            BigCard(pair: pair),
+          // SizedBox widget just takes space and doesn't render anything by itself.
+          // It's commonly used to create visual "gaps"
+          SizedBox(height: 100),
 
-            // SizedBox widget just takes space and doesn't render anything by itself.
-            // It's commonly used to create visual "gaps"
-            SizedBox(height: 100),
+          Row(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                  print('like button pressed'); // debug message
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
 
-            Row(
-              // mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ElevatedButton.icon(
-                  onPressed: () {
-                    appState.toggleFavorite();
-                    print('like button pressed'); // debug message
-                  },
-                  icon: Icon(icon),
-                  label: Text('like button'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    appState.getNext(); // get new state (current)
-                    print('next button pressed'); // debug message
-                  },
-                  child: Text('next button'),
-                ),
-              ],
-            ),
-          ],
-        ),
+              SizedBox(width: 10),
+
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext(); // get new state (current)
+                  print('next button pressed'); // debug message
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
